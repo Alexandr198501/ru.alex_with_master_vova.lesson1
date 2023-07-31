e1dd72ca57bc15c7cd83e9e0a78e076aeb51bad7
Козубняк Александр Сергеевич <alex.kozubnyak@gmail.com>
2023-08-01 00:52:32 +0400

    lesson2.1

--- src/main/java/ru/java/lesson2/HomeWork2.java
+++ src/main/java/ru/java/lesson2/HomeWork2.java
@@ -2,7 +2,9 @@ package ru.java.lesson2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++){
             System.out.print("*");
+        }
+        System.out.println();
     }
 }
