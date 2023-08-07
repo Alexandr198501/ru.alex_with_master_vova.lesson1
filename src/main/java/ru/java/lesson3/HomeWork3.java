638555a52a20ea92d3fdd091fad43df8e14266e7
Козубняк Александр Сергеевич <alex.kozubnyak@gmail.com>
2023-08-07 21:48:35 +0400

    lesson3.7

--- src/main/java/ru/java/lesson3/HomeWork3.java
+++ src/main/java/ru/java/lesson3/HomeWork3.java
@@ -8,9 +8,8 @@ public class HomeWork3 {
 
     public static void main(String[] args) {
 
-
         print1DArray();
-        //checkBalance();
+        System.out.println(checkBalance(new int[] {2, 2, 2, 1, 2, 2, 10, 1}));
 /*
 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1,
 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
@@ -23,6 +22,7 @@ public class HomeWork3 {
                 arr[i] = 1;
             }
         }
+
 /*
 2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его
 значениями 1 2 3 4 5 6 7 8 … 100;
@@ -114,17 +114,31 @@ public class HomeWork3 {
 7. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
 метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части
 массива равны.
+Примеры:
+checkBalance([2, 2, 2, 1, 2, 2, ||| 10, 1]) → true, т.е. 2 + 2 + 2 + 1 + 2 + 2 = 10 + 1
+checkBalance([1, 1, 1, ||| 2, 1]) → true, т.е. 1 + 1 + 1 = 2 + 1
  */
     public static boolean checkBalance(int[] in) {
         int sumLeft = 0;
         int sumRight = 0;
         int sum = 0;
-        for (int i = 0; i < in.length; i++){
+        int count = 0;
+        // вычисляю сумму элементов массива
+        for (int i = 0; i < in.length; i++) {
             sum += in[i];
-            sumLeft = in[i] + in[i + 1];
-            sumRight = sum - sumLeft;
         }
-        return sumLeft == sumRight; // не тестил!!!
+        // поочерёдно прибавляя к первому элементу массива последующие, проверяю, равна ли сумма левых
+        // элементов массива сумме оставшихся правых элементов массива.
+        for (int i = 0; i < in.length; i++) {
+            sumLeft += in[i];
+            sumRight = sum - sumLeft;
+            // если в массиве есть место, в котором сумма левой и правой части
+            // массива равны, увеличиваю счетчик на 1
+            if (sumLeft == sumRight){
+                count++;
+            }
+        }
+        return count > 0;
     }
 }
 
