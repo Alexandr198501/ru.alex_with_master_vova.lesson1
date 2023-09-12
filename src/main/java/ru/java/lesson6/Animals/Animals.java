9db3b937649fde24688055a7f5a30975956b4102
Козубняк Александр Сергеевич <alex.kozubnyak@gmail.com>
2023-09-12 20:34:21 +0400

    Lesson6 ДЗ к 6 уроку

---
+++ src/main/java/ru/java/lesson6/Animals/Animals.java
@@ -0,0 +1,13 @@
+package ru.java.lesson6.Animals;
+
+public abstract class Animals {
+    String name;
+    int distance;
+
+
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
+
+
+}
