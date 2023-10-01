50a738acf1da1ece12412a1960b1ac39ec37fa41
Козубняк Александр Сергеевич <alex.kozubnyak@gmail.com>
2023-10-01 18:04:43 +0400

    Lesson8 ДЗ к 8 уроку.

---
+++ src/main/java/ru/java/lesson8/HomeWork8.java
@@ -0,0 +1,11 @@
+package ru.java.lesson8;
+
+public class HomeWork8 {
+
+    // +1. Разобраться в коде игры "Угадай число".
+    // +2. В игре "Угадай число" сделать кнопку перезапуска игры.
+    // +3. * Добавить в игру 3 попытки на угадывание числа.
+    public static void main(String[] args) {
+        new MyGame();
+    }
+}
